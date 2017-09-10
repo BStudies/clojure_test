128a6c7bb739d54df1fe0f7eec4916b24e966cdf
brandon <digihew@gmail.com>
2017-09-10 01:16:13 -0400

    working on twosum problem

--- my-stuff/src/my_stuff/core.clj
+++ my-stuff/src/my_stuff/core.clj
@@ -113,10 +113,6 @@
     (f)
 )
 (triplicate greeting)
-(defn triplicate [f] (fn [f] (dotimes [n 3] f)))
-(defn triplicate [f] ((f) (f) (f)))
-
-(triplicate (greet))
 
 
 
@@ -154,11 +150,145 @@
 ; function you defined in the 
 ; earlier triplicate exercise.
 
-(defn triplicate2 "trip2" [f & args]
-  (f & args)
-  (triplicate [& f args])
-)
-(triplicate2 greeting "test")
+; not sure about this one
+; (defn triplicate2 "trip2" [f]
+;   (println "Executing f")
+;   (println f)
+;   (println args)
+;   (fn [args] (f args))
+;   ; [f & args]
+;   ; (triplicate [& f args])
+; )
+; (triplicate2 greeting "test")
+
+
+; 10) Using the java.lang.Math 
+; class (Math/pow, Math/cos, Math/sin, Math/PI), 
+; demonstrate the following mathematical facts: * 
+; The cosine of pi is -1 *
+;  For some x, sin(x)^2 + cos(x)^2 = 1
+(= -1.0 (Math/cos Math/PI))
+(def x 10)
+(def y (Math/pow (Math/sin x) 2))
+(def z (Math/pow (Math/cos x) 2))
+(= 1.0 (+ y z))
+
+
+; 11) Define a function that takes an 
+; HTTP URL as a string, fetches that URL 
+; from the web, and returns the content as a string.
+
+; (def webText (java.net.URL/openStream "http://www.w3.org"))
+; (def url (java.net.URL "http://www.w3.org"))
+
+; (java.net.URL/constructor "http://www.w3.org")
+
+; (java.net.URL. "url")
+; (.java.net.URL rnd)
+; (def webText (java.net.URL/openConnection "http://www.w3.org"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; two sum, given an array of integers, return indecies of
+; of two numbers that add to target
+; (def myVec [1 2 3 4 5 6 7 8 9])
+; loop through array
+; (into #{} arr)
+
+
+
+; builds a map of numbers and their opposites based on a sum value
+
+; (use 'clojure.contrib.trace)
+
+
+; (defn buildMapSumOpposites [myVec sumValue] (
+;   (def myHash {})
+;   (println "created hash hit loop")
+;   (println myHash)
+;   ; (for [x myVec] (def myHash (assoc myHash x (- sumValue x))))
+;   (def myHash (assoc myHash 3 (- sumValue 3)))
+;   (println myHash)
+;   ; (println myHash)
+;   identity myHash
+; ))
+
+
+
+; converts vector to map of elements and indicies 
+(defn buildMap [vec] (
+  (def myMap {})
+  (doseq [[i e] (map-indexed vector vec)] 
+    (def myMap (assoc myMap e i)))
+  (identity myMap)
+))
+
+(assert (= {3 0, 2 1, 1 2} (buildMap [3 2 1])) "build map")
+
+
+
+; (defn buildMapSumOpposites [vec sumVal] (
+;   (def myMap {})
+;   (doseq [e vec] 
+;     (def myMap (assoc myMap e (- sumVal e))))
+;   ; (println myMap)
+;   (identity myMap)
+; ))
+; (assert (= {3 1, 2 2, 1 3} (buildMapSumOpposites [3 2 1] 4)))
+
+
+
+
+
+; (dotrace [buildMapSumOpposites] (buildMapSumOpposites [1 2 3] 4))
+
+; ; searches for index of two values that sum to given
+; (defn findSumPair [myVec myHash sumValue] (
+;   (doseq myVec)
+; ))
+; (assert (= [0 1] (buildMapSumOpposites [3 2 1] {3 1, 2 2, 1 3} 4)))
+
+
+(defn findPair [myHash sumVal] (
+  (def myHashKeys (keys myHash))
+  ; (println myHashKeys)
+  ; (def opposites [])
+  (def indicies [])
+  (doseq [k myHashKeys] 
+    (do 
+      (def opposite (- sumVal k))
+      (println opposite)
+      (if (get myHash opposite) (conj indicies (get myHash k)))
+    )
+  )
+  (println indicies)
+))
+(assert (= [0 1] (findPair (buildMap [3 2 1]) 4)))
+
+
+; (defn twoSum [vec sumValue] (
+;   (def myHash (buildMap vec))
+;   identity (findPair vec myHash sumValue)
+;   )
+; )
+; (assert (= [0 1] (twoSum [3 2 1] 4)))
+
+; (def myVec [1 2 3 4 5 6 7 8 9])
+; (def sumVal 10)
+; (twoSum myVec sumVal)
+
+
 
 
 
